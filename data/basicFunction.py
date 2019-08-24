10f7a6158644f975b47c05e5db9681956bb23c42
vious <viousxie@outlook.com>
2019-08-24 14:11:16 +0800

    dataloader

--- data/basicFunction.py
+++ data/basicFunction.py
@@ -17,3 +17,13 @@ def MaskTransform(cropSize):
         Resize(size=cropSize, interpolation=Image.NEAREST),
         ToTensor(),
     ])
+
+# this was image transforms function for paired image and mask, which means that damaged image and the 
+# mask are in pairs, the input image already contains damaged area with (ones or zeros),
+#  we suggest that you resize the input image with "NEAREST" not BICUBIC(or other) algorithm,
+#  is is not guaranteed, but in some cases, the damaged portion might go out of the mask region, if you perform other resize methods
+def PairedImageTransform(cropSize):
+    return Compose([
+        Resize(size=cropSize, interpolation=Image.NEAREST),
+        ToTensor(),
+    ])
--- data/pairedData.py
+++ data/pairedData.py
@@ -6,6 +6,7 @@ from os.path import join
 from random import randint
 from data.basicFunction import CheckImageFile, ImageTransform, MaskTransform
 
+#
 class GetPariedData(Dataset):
     def __init__(self, dataRoot, maskRoot, imgPrefix, maskPrefix, loadSize, cropSize):
         super(GetPariedData, self).__init__()
